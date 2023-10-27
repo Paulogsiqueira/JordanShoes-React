fc75e751de451ec80649e63a67345a7c86d075e4
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-10-27 15:18:29 -0300

    Melhorias carrinho de compras

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,6 +5,18 @@ import bin from '../../img/icon/bin.png'
 
 const Cart = () => {
   const { cart, setCart } = useContext(CartContext)
+  const [total, setTotal] = useState(0);
+  const [freight, setFreight] = useState(0);
+  const [cep, setCep] = useState("");
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalPrice += cart[i].price * cart[i].quantity;
+    }
+    setTotal(totalPrice)
+  }, [cart])
 
   const addQuantity = (index) => {
     const updatedCart = [...cart]
@@ -29,68 +41,113 @@ const Cart = () => {
     setCart(updatedCart);
   }
 
+  const changeCep = (e) => {
+    let value = e.target.value
+    if ((/^\d+$/.test(value) && value.length <= 8) || value == "") {
+      setCep(value)
+    } else {
+      console.log("Nao numerico")
+    }
+  }
+  const calcFreight = () => {
+    if (cep.length == 8) {
+      setError("")
+      setFreight(Math.random() * (30 - 15) + 15)
+      console.log(freight)
+    } else {
+      setError("CEP informado é inválido")
+    }
+  }
+
   return (
     <div>
-      <h2>Meu carrinho</h2>
-      <div className='cart-page'>
-        <div className='cart-itens'>
-          <ul>
-            {cart.map((product, index) => (
-              <li key={index} >
-                <div className='cart-item'>
-                  <section className='item-img'>
-                    <img src={product.img} alt="Foto do tenis selecionado" />
-                  </section>
-                  <section className='item-desc'>
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                    <p><strong>Marca:</strong>Nike</p>
-                  </section>
-                  <section className='item-options'>
-                    <div className='item-options__quantity'>
-                      <div className='quantity-title'>
-                        Qtd
+      {cart.length > 0 ? <div className='cart-body'>
+        <h2 className='cart-title'>Carrinho de Compras</h2>
+        <div className='cart-page'>
+          <div className='cart-itens'>
+            <ul>
+              {cart.map((product, index) => (
+                <li key={index} >
+                  <div className='cart-item'>
+                    <section className='item-img'>
+                      <img src={product.img} alt="Foto do tenis selecionado" />
+                    </section>
+                    <section className='item-desc'>
+                      <h3>{product.name}</h3>
+                      <p>{product.description}</p>
+                      <p><strong>Marca: </strong>Nike</p>
+                    </section>
+                    <section className='item-options'>
+                      <div className='item-options__quantity'>
+                        <div className='quantity-title'>
+                          Qtd
+                        </div>
+                        <div className='item-options__btn'>
+                          <p className='btn-quantity' onClick={() => (decreaseQuantity(index))}>-</p>
+                          <p className='item-quantity'>{product.quantity}</p>
+                          <p className='btn-quantity' onClick={() => (addQuantity(index))}>+</p>
+                        </div>
                       </div>
-                      <div className='item-options__btn'>
-                        <p className='btn-quantity' onClick={() => (decreaseQuantity(index))}>-</p>
-                        <p className='item-quantity'>{product.quantity}</p>
-                        <p className='btn-quantity' onClick={() => (addQuantity(index))}>+</p>
+                      <div className='item-options__price'>
+                        <div className='price-title' >
+                          Preço
+                        </div>
+                        <div className='item-price'>
+                          <p>R$ {(product.quantity * product.price).toFixed(2)}</p>
+                        </div>
                       </div>
-                    </div>
-                    <div className='item-options__price'>
-                      <div className='price-title' >
-                        Preço
+                      <div className='delete-item'>
+                        <img src={bin} alt="Lixeira" onClick={() => (deleteItem(index))} />
                       </div>
-                      <div className='item-price'>
-                        <p>R$ {product.quantity * product.price}</p>
-                      </div>
-                    </div>
-                    <div className='delete-item'>
-                      <img src={bin} alt="Lixeira" onClick={() => (deleteItem(index))} />
-                    </div>
-                  </section>
-                </div>
-              </li>
-            ))}
-          </ul>
+                    </section>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='payment-itens'>
+            <div className='payment-cart'>
+              <h2 className='payment-resume'>Resumo</h2>
+              <div className='payment-subtotal'>
+                <p>Subtotal</p>
+                <p className='subtotal-value'>R$ {(total).toFixed(2)}</p>
+              </div>
+              {freight != 0 && <div className='freight'>
+                <p>Frete</p>
+                <p >R$ {(freight).toFixed(2)}</p>
+              </div>
+              }
+              {freight != 0 && <div className='payment-total'>
+                <p>Total</p>
+                <p className='payment-value'>R$ {(total+ freight).toFixed(2) }</p>
+              </div>}
+              <div className='payment-pix'>
+                <p className='pix-method'>à vista</p>
+                <h2>R$ {((total+ freight) * 0.9).toFixed(2)}</h2>
+                <p className='pix-discount'>no PIX com 10% de desconto</p>
+              </div>
+              <div className='card-method'>
+                <h2>R$ {(total+ freight).toFixed(2)}</h2>
+                <p>em até 6x de R$ {((total + freight)/6).toFixed(2)} sem juros no cartão</p>
+              </div>
+              <button className='btn-cart'>Finalizar Pedido</button>
+            </div>
+            <div className='payment-freight'>
+              <h2 className='payment-resume'>Calcular frete</h2>
+              <label>
+                CEP
+                <input type='text' value={cep} onChange={(e) => { changeCep(e) }}></input>
+              </label>
+              {freight != 0 && <h4 className='freight-value'>Valor do frete: R$ {freight.toFixed(2)}</h4>}
+              {error != "" && <h4 className='freight-erro'>{error}</h4>}
+              <button className='btn-cart' onClick={calcFreight}>Calcular</button>
+            </div>
+          </div>
         </div>
-        <div className='payment-cart'>
-          <h2 className='payment-resume'>Resumo</h2>
-          <div className='payment-total'>
-            <p>Total</p>
-            <p>R$ 2500.00</p>
-          </div>
-          <div className='payment-pix'>
-            <p className='pix-method'>à vista</p>
-            <h2>R$ 2250.00</h2>
-            <p className='pix-discount'>no PIX com 10% de desconto</p>
-          </div>
-          <div className='card-method'>
-            <h2>R$ 2250.00</h2>
-            <p>em até 6x de R$ 200 sem juros no cartão</p>
-          </div>
-        </div>
-      </div>
+      </div> : <div className='empity-cart'>
+        <h2>Carrinho vazio</h2>
+        <p> Volte a nossa página incial e escolha os produtos que mais combinam com você!</p>
+      </div>}
 
     </div>
   )
