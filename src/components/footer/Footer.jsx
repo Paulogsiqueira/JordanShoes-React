518a8c5c015142eab245edfdd56a349b179b1021
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-11-07 13:38:25 -0300

    Alteração footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
+import linkedin from '../../img/icon/linkedin.png'
+import github from '../../img/icon/github.png'
 import './Footer.css'
+
 
 const Footer = () => {
   return (
     <footer>
-        Todos os direitos reservados 
+      <div className='footer-links'>
+        <div className='footer-links__linkedin'><a><img src={linkedin} alt="Icone para LinkedIn"/></a></div>
+        <div className='footer-links__github'><img src={github} alt="Icone para GitHub" /></div>
+      </div>
+      <div className='footer-text'>
+        <p> © 2023 Paulo Gustavo Siqueira</p>
+      </div>
     </footer>
   )
 }
