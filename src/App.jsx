42b02f987c76f3c5990838b00a04d3e5881bea82
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-11-08 12:03:55 -0300

    Limitação de acessos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route , Navigate} from 'react-router-dom'
+import { LoginContext } from './context/LoginContext.jsx'
+import { useState,useContext } from 'react'
 import NavBar from './components/navbar/Navbar.jsx'
 import Home from './pages/home/Home'
 import Register from './pages/register/Register'
@@ -9,9 +10,12 @@ import Login from './pages/login/Login'
 import Product from './pages/product/Product'
 import Cart from './pages/cart/Cart'
 import PaymentComplete from './pages/paymentComplete/PaymentComplete'
+import './App.css'
+
 
 
 function App() {
+  const {login, setLogin} = useContext(LoginContext)
 
   return (
     <>
@@ -20,11 +24,11 @@ function App() {
           <Routes>
             <Route path='/product/:id' element={<Product/>} />
             <Route path='/' element={<Home/>} />
-            <Route path='/cart' element={<Cart/>} />
+            <Route path='/cart' element={login ? <Cart/> : <Navigate to="/" />} />
             <Route path='/login' element={<Login/>} />
             <Route path='/register' element={<Register />} />
-            <Route path='/payment' element={<FinishOrder/>} />
-            <Route path='/paymentComplete' element={<PaymentComplete/>} />
+            <Route path='/payment' element={ login ? <FinishOrder/> : <Navigate to="/" />} />
+            <Route path='/paymentComplete' element={login ? <PaymentComplete/> : <Navigate to="/" />} />
           </Routes>
         <Footer />
       </BrowserRouter>
