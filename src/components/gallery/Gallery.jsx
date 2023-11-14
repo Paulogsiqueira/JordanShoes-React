c202e3a1af67f50bbe75db486e65553b28f9b329
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-11-14 12:04:54 -0300

    Melhorias no código

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react'
 import { CardsList } from './CardsList/CardsList'
 import { Pagination } from '@mui/material'
+import { products } from '@/data/products.jsx'
 import './Gallery.css'
 
 const Gallery = () => {
     const [page, setPage] = useState(1)
-    const [count, setCount] = useState(2)
+    const [count, setCount] = useState(Math.ceil(products.length/8));
 
     const handlePagination = (value) => {
         setPage(value)
     }
 
     const changeCount = (value) => {
-        setCount(value)
+        setCount(() => value)
     }
 
     return (
         <div className='products'>
-            <CardsList page={page} changeCount={changeCount} />
-            <Pagination count={count} variant="outlined" color="primary" className='pagination' onChange={(e, value) => handlePagination(value)} />
+            <CardsList page={page} changeCount={changeCount} products={products} />
+            <Pagination count={count} variant="outlined" color="primary" className='pagination' page={page} onChange={(e, value) => handlePagination(value)} />
         </div>
     )
 }
